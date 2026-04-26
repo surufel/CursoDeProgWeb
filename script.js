f7fd16f070a549261cfc0e912d078ad71a1d38c7
Jose Landim <joselandimsobrinho@hotmail.com>
2026-04-26 17:32:29 -0300

    feat: adicionando a api do github

--- script.js
+++ script.js
@@ -21,3 +21,50 @@ function mostrarMatricula(event) {
 
 if (botaoNav) botaoNav.addEventListener('click', mostrarMatricula);
 if (botaoHero) botaoHero.addEventListener('click', mostrarMatricula);
+
+//api do github
+async function buscarRepositorios() {
+  const url = 'https://api.github.com/users/Surufel/repos'
+  const resposta = await fetch(url)
+  const repositorios = await resposta.json()
+
+  //qtd total de repo
+  const quantidade = repositorios.length
+  document.getElementById('github_repo-count').textContent = quantidade
+
+  //qtd total de estrelas
+  let totalEstrelas = 0
+  repositorios.forEach(repo => {
+    totalEstrelas += repo.stargazers_count
+  })
+  document.getElementById('github_stars-count').textContent = totalEstrelas
+
+  //qtd de linguagens
+  let linguagensUnicas = []
+  repositorios.forEach(function(projeto) {
+    let linguagemDoProjeto = projeto.language
+    if (linguagemDoProjeto != null && !linguagensUnicas.includes(linguagemDoProjeto)) {
+      linguagensUnicas.push(linguagemDoProjeto)
+    }
+  })
+  document.getElementById('github_language-count').textContent = linguagensUnicas.length
+
+  //construindo os cartões
+  let htmlCompleto = "";
+  repositorios.forEach(function(projeto) {
+    // Definimos um texto padrão caso o projeto não tenha descrição
+    let descricao = projeto.description !== null ? projeto.description : "Sem descrição no momento.";
+    
+    let htmlDoCartao = `
+      <div class="gh-repos__card">
+        <h3>${projeto.name}</h3>
+        <p>${descricao}</p>
+      </div>
+    `;
+    htmlCompleto += htmlDoCartao;
+  });
+  
+  document.getElementById('gh-repos-grid').innerHTML = htmlCompleto;
+}
+
+buscarRepositorios()
